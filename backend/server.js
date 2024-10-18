3cb31540bd01e08db9c4daaf3edc3de9ab9cb8d5
georgeyegon <georgeyegon67@gmail.com>
2024-10-18 12:47:22 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const express = require('express');
+const path = require('path');
 
-const server = jsonServer.create()
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('/', router)
+server.use('/resources', express.static(path.join(__dirname, '../resources')));
+
+server.use(middlewares);
+server.use('/', router);
+
 server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
